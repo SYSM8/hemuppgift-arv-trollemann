f14e890838cb95b5dd0e56c56aa2fa0a37f9fc48
benjamin burton <benjamin.alexander.burton@gmail.com>
2024-10-14 13:05:34 +0200

    tror jag e klar med player för nu

---
+++ Hemuppgift_Arv_Temp/Game/Player.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hemuppgift_Arv_Temp.Game
+{
+    public abstract class Player
+    {
+        //property
+        private string userId;
+        public string UserId
+        {
+            get { return userId; }
+            set { userId = value; }
+        }
+
+        //constructor
+        public Player(string userId)
+        {
+            UserId = userId;
+        }
+
+        //method
+        //returnerar spelarens namn
+        public string GetUserId()
+        {
+            return userId;
+        }
+
+        //abstract method
+        //ärver från underklasserna HumanPlayer och ComputerPlayer
+        public abstract int TakePins (Board board);
+    }
+}
