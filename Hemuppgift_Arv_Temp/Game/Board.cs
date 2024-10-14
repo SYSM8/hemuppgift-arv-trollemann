914efff7ed34406222551f1fec1da2f685c87959
benjamin burton <benjamin.alexander.burton@gmail.com>
2024-10-14 12:35:34 +0200

    tror jag r klar med board klassen

---
+++ Hemuppgift_Arv_Temp/Game/Board.cs
@@ -0,0 +1,57 @@
+﻿namespace Hemuppgift_Arv_Temp.Game
+{
+    public class Board
+    {
+        // property (egenskap/attribut)
+        private int noPins;
+        public int NoPins
+        {
+            get { return noPins; }
+            set { noPins = value; }
+        }
+
+        // metod för att tilldela en vald mängd stickor till noPins
+        public void SetUp(int setUp)
+        {
+            if (setUp > 0)
+            {
+                NoPins = setUp;
+                Console.WriteLine($"amount of pins added: {NoPins}");
+            }
+            else
+            {
+                Console.WriteLine("\namount needs to be more than 0");
+            }
+        }
+
+
+        // metod för att ta stickor från "bordet"
+        int takePins;
+        public void TakePins(int takePins)
+        {
+            Console.Write("take 1 or 2 pins: ");
+            takePins = Convert.ToInt32(Console.ReadLine());
+
+            if (takePins < 1 || takePins > 2)
+            {
+                Console.WriteLine("can only take 1 or two pins");
+                return;
+            }
+
+            if (NoPins < takePins)
+            {
+                Console.WriteLine("not enough pins left.");
+                return;
+            }
+
+            // minska antalet stickor i spelet
+            NoPins -= takePins;
+            GetNoPins();
+        }
+
+        public void GetNoPins()
+        {
+            Console.WriteLine($"amount of pins left: {NoPins}");
+        }
+    }
+}
--- Hemuppgift_Arv_Temp/Game/TakePins.cs
+++ Hemuppgift_Arv_Temp/Game/TakePins.cs
@@ -5,8 +5,14 @@
         //Här är main klassen där koden ska testas, lägg in i mappen
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            Board board = new Board();
 
+            // Fråga användaren hur många stickor som ska finnas på bordet
+            Console.Write("welcome!\ninput amount of sticks to play with: ");
+            int setUp = Convert.ToInt32(Console.ReadLine());
+
+            board.SetUp(setUp);
+        }
     }
 }
+    
